117fba0e726c25246b260dcb0d71e1b2ee394495
Matthias Noback <matthiasnoback@gmail.com>
2020-06-05 15:22:41 +0200

    Chapter 2

---
+++ public/random.php
@@ -0,0 +1,17 @@
+<?php $randomInt = random_int(1, 10); ?>
+<html lang="en">
+<head>
+    <title>Your lucky number</title>
+</head>
+<body>
+<h1>Your lucky number is: <?php echo $randomInt; ?></h1>
+<?php if ($randomInt > 5) { ?>
+    <h2>Nice!</h2>
+<?php } ?>
+<p>
+    <a href="/kittens.php?number=<?php echo $randomInt; ?>">
+        Now show me <?php echo $randomInt; ?> kittens!
+    </a>
+</p>
+</body>
+</html>
